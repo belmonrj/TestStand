138cb03c3cb28566d65da05a28716eb0947f97c3
Ron Belmont <belmonrj@gmail.com>
2015-08-19 11:45:12 -0600

    Some cosmetic changes and improved output figure names to analyzerV2_mean

--- analyzerV2_mean.C
+++ analyzerV2_mean.C
@@ -1,59 +1,28 @@
-// //==========================================================================
-// // HCalTestStand - program to analyze SiPM Source Readout Tests
-// // CVS version control on Colorado local cluster
-// //
-// // authors:  Shawn Beckman - Sebastian Seeds
-// //==========================================================================
+//==========================================================================
+// HCalTestStand - program to analyze SiPM Source Readout Tests
+// CVS version control on Colorado local cluster
+//
+// authors:  Shawn Beckman - Sebastian Seeds
+//==========================================================================
 
-// // STL
-// #include <iostream>
-// #include <fstream>
-// #include <string>
-// #include <vector>
-// // C
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <time.h>
-// #include <sys/stat.h>
-// #include <sys/types.h>
-// #include <string.h>
-// // ROOT
-// #include <TAxis.h>
-// #include <TPad.h>
-// #include <TFile.h>
-// #include <TH1.h>
-// #include <TH1D.h>
-// #include <TH2D.h>
-// #include <TNtuple.h>
-// #include <TCanvas.h>
-// #include <TGraphErrors.h>
-// #include <TF1.h>
-// #include <TROOT.h>
-// #include <TStyle.h>
-// #include <TMath.h>
-// #include <TSpectrum.h>
-// #include <TList.h>
-// #include <TPaveStats.h>
-// #include <TGaxis.h>
-// #include <TRandom.h>
-// #include <TSystem.h>
-// #include <TProfile.h>
-// #include <TObject.h>
-// #include <TString.h>
+// Modified by Ron Belmont
+// local version control with git, will add to github repository
 
-////////////////////////////////////////////////////////////////////
-// organizing files, and defining things before running over data //
-////////////////////////////////////////////////////////////////////
+
 
 void analyzerV2_mean()
 {
 
-  analyze("A1_LED_ColTests_Fiber3_VMIN_SIPM1.txt","A1_LED_ColTests_Fiber3_TIME.txt",true);
+  analyze("A1_LED_ColTests_Fiber1_VMIN_SIPM1","A1_LED_ColTests_Fiber1_TIME.txt",true);
+  analyze("A1_LED_ColTests_Fiber2_VMIN_SIPM1","A1_LED_ColTests_Fiber2_TIME.txt",true);
+  analyze("A1_LED_ColTests_Fiber3_VMIN_SIPM1","A1_LED_ColTests_Fiber3_TIME.txt",true);
+  analyze("A1_LED_ColTests_Fiber4_VMIN_SIPM1","A1_LED_ColTests_Fiber4_TIME.txt",true);
 
 }
 
 
-void analyze(const char* datafile, const char* timedata, bool PEConvert = true)
+
+void analyze(const char* NAME, const char* timedata, bool PEConvert = true)
 {
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -83,10 +52,7 @@ void analyze(const char* datafile, const char* timedata, bool PEConvert = true)
   ifstream file;
   ifstream file_cnt;
 
-  int PEmult = 1;
-  if ( PEConvert ) PEmult = -1;
-
-  file.open(datafile);
+  file.open(Form("%s.txt",NAME));
 
   for(int i = 0; i < totalBins ; i++)
     {
@@ -96,7 +62,8 @@ void analyze(const char* datafile, const char* timedata, bool PEConvert = true)
       // means.push_back(-1*meanval_d);
       double meanval_d = -9999;
       file>>meanval_d;
-      means.push_back(PEmult*meanval_d);
+      if ( PEConvert) means.push_back(-1*meanval_d);
+      else means.push_back(meanval_d);
     }
 
   file.close();
@@ -131,30 +98,29 @@ void analyze(const char* datafile, const char* timedata, bool PEConvert = true)
 
   double TimeSum = 0;
 
-  for(int i=0; i<timeSize; i++){  //Gets total time for first column
+  for ( int i=0; i<timeSize; i++ )
+    {  //Gets total time for first column
 
-    int row = i%scan_nypositions;
-    int column = i/scan_nypositions;
+      int row = i%scan_nypositions;
+      int column = i/scan_nypositions;
 
-
-    if (column == 1){
-      TimeSum += times[i];
-
+      if (column == 1) TimeSum += times[i];
     }
-  }
 
   double TimeMean = TimeSum/scan_nypositions;
 
   ////////////////////////////////////////////////////////////
-  char Name[] = "meanHistSub"; //Name the saved histogram HERE
+  //char NAME[] = "meanHistSub"; //Name the saved histogram HERE
   ////////////////////////////////////////////////////////////
 
   double distanceX = (double)scan_nxpositions/2; //For 2 steps per cm. Check Stepper code for agreement
   double distanceY = (double)scan_nypositions/2;
 
-  TH2D *meanHist = new TH2D("meanHist", "meanHist", scan_nxpositions,0.0,distanceX, scan_nypositions,0.0,distanceY); // Modify numbers for cm scale on panel.
+  TH2D *meanHist = new TH2D(Form("%s_meanHist",NAME), Form("%s_meanHist",NAME),
+			    scan_nxpositions,0.0,distanceX, scan_nypositions,0.0,distanceY); // Modify numbers for cm scale on panel.
 
-  TH2D *meanHistSub = new TH2D(Form("%s",Name), Form("%s",Name), scan_nxpositions,0.0,distanceX, scan_nypositions,0.0,distanceY); // Modify numbers for cm scale on panel.
+  TH2D *meanHistSub = new TH2D(Form("%s_meanHistSub",NAME), Form("%s_meanHistSub",NAME),
+			       scan_nxpositions,0.0,distanceX, scan_nypositions,0.0,distanceY); // Modify numbers for cm scale on panel.
 
   for(int i = 0; i < meanSize; i++)
     {
@@ -195,7 +161,7 @@ void analyze(const char* datafile, const char* timedata, bool PEConvert = true)
       meanHistSub->SetBinContent(column + 1, row + 1, iMeanSubPE);
     }
 
-  meanHistSub->SaveAs(Form("%s.root",Name));
+  meanHistSub->SaveAs(Form("%s_meanHistSub.root",NAME)); // NEEDS TO BE REVISED
 
   TCanvas *c1 = new TCanvas("c1","c1", 900, 588);
   TCanvas *c2 = new TCanvas("c2","c2", 900, 588);
@@ -207,13 +173,8 @@ void analyze(const char* datafile, const char* timedata, bool PEConvert = true)
   meanHist->SetTitle(Form("Mean %d events",statistics));
   meanHist->GetXaxis()->SetTitle("Position Y (cm)");
   meanHist->GetYaxis()->SetTitle("Position X (cm)");
-  if (PEConvert == true)
-    {
-      meanHist->GetZaxis()->SetTitle("Pulse Min Value (photoelectrons)");
-    }else
-    {
-      meanHist->GetZaxis()->SetTitle("Pulse Min Value (V)");
-    }
+  if (PEConvert == true) meanHist->GetZaxis()->SetTitle("Pulse Min Value (photoelectrons)");
+  else meanHist->GetZaxis()->SetTitle("Pulse Min Value (V)");
   meanHist->Draw("colz");
 
   c2->cd();
@@ -221,16 +182,14 @@ void analyze(const char* datafile, const char* timedata, bool PEConvert = true)
   meanHistSub->SetTitle(Form("Mean %d events - Background Subtracted",statistics));
   meanHistSub->GetXaxis()->SetTitle("Position Y (cm)");
   meanHistSub->GetYaxis()->SetTitle("Position X (cm)");
-  if (PEConvert == true)
-    {
-      meanHistSub->GetZaxis()->SetTitle("Pulse Min Value (photoelectrons)");
-    }else
-    {
-      meanHistSub->GetZaxis()->SetTitle("Pulse Min Value (V)");
-    }
+  if (PEConvert == true) meanHistSub->GetZaxis()->SetTitle("Pulse Min Value (photoelectrons)");
+  else meanHistSub->GetZaxis()->SetTitle("Pulse Min Value (V)");
   meanHistSub->Draw("colz");
 
-  c1->Print("canvas1.png");
-  c2->Print("canvas2.png");
+  c1->Print(Form("%s_meanHist.png",NAME));
+  c2->Print(Form("%s_meanHistSub.png",NAME));
+
+  delete c1;
+  delete c2;
 
 }
