053e6c34d0400ac62f506ad481fef9c3da958596
Ron Belmont <belmonrj@gmail.com>
2015-08-19 11:18:01 -0600

    Updated analyzerV2_mean to not need to be compiled

--- analyzerV2_mean.C
+++ analyzerV2_mean.C
@@ -1,65 +1,76 @@
-//==========================================================================
-// HCalTestStand - program to analyze SiPM Source Readout Tests
-// CVS version control on Colorado local cluster 
-// 
-// authors:  Shawn Beckman - Sebastian Seeds
-//==========================================================================
+// //==========================================================================
+// // HCalTestStand - program to analyze SiPM Source Readout Tests
+// // CVS version control on Colorado local cluster
+// //
+// // authors:  Shawn Beckman - Sebastian Seeds
+// //==========================================================================
 
-// STL                                                                                                         
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-// C                                                                                                           
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <string.h>
-// ROOT
-#include <TAxis.h>
-#include <TPad.h>
-#include <TFile.h>
-#include <TH1.h>
-#include <TH1D.h>
-#include <TH2D.h>
-#include <TNtuple.h>
-#include <TCanvas.h>
-#include <TGraphErrors.h>
-#include <TF1.h>
-#include <TROOT.h>
-#include <TStyle.h>
-#include <TMath.h>
-#include <TSpectrum.h>
-#include <TList.h>
-#include <TPaveStats.h>
-#include <TGaxis.h>
-#include <TRandom.h>
-#include <TSystem.h>
-#include <TProfile.h>
-#include <TObject.h>
-#include <TString.h>
+// // STL
+// #include <iostream>
+// #include <fstream>
+// #include <string>
+// #include <vector>
+// // C
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <time.h>
+// #include <sys/stat.h>
+// #include <sys/types.h>
+// #include <string.h>
+// // ROOT
+// #include <TAxis.h>
+// #include <TPad.h>
+// #include <TFile.h>
+// #include <TH1.h>
+// #include <TH1D.h>
+// #include <TH2D.h>
+// #include <TNtuple.h>
+// #include <TCanvas.h>
+// #include <TGraphErrors.h>
+// #include <TF1.h>
+// #include <TROOT.h>
+// #include <TStyle.h>
+// #include <TMath.h>
+// #include <TSpectrum.h>
+// #include <TList.h>
+// #include <TPaveStats.h>
+// #include <TGaxis.h>
+// #include <TRandom.h>
+// #include <TSystem.h>
+// #include <TProfile.h>
+// #include <TObject.h>
+// #include <TString.h>
 
 ////////////////////////////////////////////////////////////////////
 // organizing files, and defining things before running over data //
 ////////////////////////////////////////////////////////////////////
 
-int analyzerV2_mean(const char* datafile, const char* timedata, bool PEConvert = true) {
+void analyzerV2_mean()
+{
 
-////////////////////////////////////////////////////////////////////////////////
-//MODIFY FOR SIZE OF PANEL. CHECK TOTAL FILES MANUALLY FOR COMPATIBILITY HERE.// 
-////////////////////////////////////////////////////////////////////////////////
+  analyze("A1_LED_ColTests_Fiber3_VMIN_SIPM1.txt","A1_LED_ColTests_Fiber3_TIME.txt",true);
 
-  int statistics = 1500; //Total pulse events per location 
-  
-  int scan_nxpositions = 58; 
-  int scan_nypositions = 9; 
+}
 
-  double PE = .004386; //Measured value of PE peak spacing.
+
+void analyze(const char* datafile, const char* timedata, bool PEConvert = true)
+{
+
+  ////////////////////////////////////////////////////////////////////////////////
+  //MODIFY FOR SIZE OF PANEL. CHECK TOTAL FILES MANUALLY FOR COMPATIBILITY HERE.//
+  ////////////////////////////////////////////////////////////////////////////////
+
+  int statistics = 1500; //Total pulse events per location
+
+  // CHECK THESE WHEN USING
+  int scan_nxpositions = 58;
+  int scan_nypositions = 9;
+
+  // ADJUST AS NEEDED
+  double PE = .004386; //Measured value of PE peak spacing. -- // this is SiPM 1
 
   int totalBins = scan_nxpositions * scan_nypositions;
-  
+
   double background = 0.0; //Defining for noise subtraction
 
   ////////////////////////////////////////////
@@ -68,101 +79,110 @@ int analyzerV2_mean(const char* datafile, const char* timedata, bool PEConvert =
 
   int numb = 0;
   vector <double> means;
-  string line;  
+  string line;
   ifstream file;
   ifstream file_cnt;
 
-  file.open(Form("%s",datafile));
-  
+  int PEmult = 1;
+  if ( PEConvert ) PEmult = -1;
+
+  file.open(datafile);
+
   for(int i = 0; i < totalBins ; i++)
     {
-      string meanval_s;
-      getline(file,meanval_s);
-      double meanval_d = stod(meanval_s);
-      means.push_back(-1*meanval_d);
+      // string meanval_s;
+      // getline(file,meanval_s);
+      // double meanval_d = std::stod(meanval_s);
+      // means.push_back(-1*meanval_d);
+      double meanval_d = -9999;
+      file>>meanval_d;
+      means.push_back(PEmult*meanval_d);
     }
-  
+
   file.close();
-      
+
   int meanSize = means.size();
 
   /////////////////////////////////////////////////////////////////////////////
   //////////////////// Getting Average background Rate ////////////////////////
   /////////////////////////////////////////////////////////////////////////////
-  
+
   int timenumb = 0;
   vector <double> times;
-  string timeline;  
+  string timeline;
   ifstream timefile;
-      
-  timefile.open(Form("%s",timedata));
-  
+
+  timefile.open(timedata);
+
   for(int i = 0; i < totalBins ; i++)  //Gets time data
     {
-      string timeval_s;
-      getline(timefile,timeval_s);
-      double timeval_d = stod(timeval_s);
+      // string timeval_s;
+      // getline(timefile,timeval_s);
+      // double timeval_d = std::stod(timeval_s);
+      // times.push_back(timeval_d);
+      double timeval_d = -9999;
+      timefile>>timeval_d;
       times.push_back(timeval_d);
     }
-  
+
   timefile.close();
-      
+
   int timeSize = times.size();
 
   double TimeSum = 0;
-  
+
   for(int i=0; i<timeSize; i++){  //Gets total time for first column
-    
+
     int row = i%scan_nypositions;
     int column = i/scan_nypositions;
-    
-    
+
+
     if (column == 1){
       TimeSum += times[i];
-      
+
     }
   }
-  
+
   double TimeMean = TimeSum/scan_nypositions;
-  
+
   ////////////////////////////////////////////////////////////
   char Name[] = "meanHistSub"; //Name the saved histogram HERE
   ////////////////////////////////////////////////////////////
 
-  double distanceX = scan_nxpositions/2; //For 2 steps per cm. Check Stepper code for agreement
-  double distanceY = scan_nypositions/2;
-    
+  double distanceX = (double)scan_nxpositions/2; //For 2 steps per cm. Check Stepper code for agreement
+  double distanceY = (double)scan_nypositions/2;
+
   TH2D *meanHist = new TH2D("meanHist", "meanHist", scan_nxpositions,0.0,distanceX, scan_nypositions,0.0,distanceY); // Modify numbers for cm scale on panel.
-  
+
   TH2D *meanHistSub = new TH2D(Form("%s",Name), Form("%s",Name), scan_nxpositions,0.0,distanceX, scan_nypositions,0.0,distanceY); // Modify numbers for cm scale on panel.
-  
+
   for(int i = 0; i < meanSize; i++)
     {
-      
+
       int row = i%scan_nypositions;
       int column = i/scan_nypositions;
       double iMean = means[i];
-      
+
       if(column == 1)
 	{
 	  background += iMean;  // Gets total event data for first column
 	}
-      
+
       meanHist->SetBinContent(column+1,row+1,iMean);  //Fills unsubtracted histogram
       if (PEConvert == true)
 	{
 	  meanHist->SetBinContent(column+1,row+1,iMean/PE);
 	}
     }
-    
+
   background = background/scan_nypositions;
   double AvgBackgroundRate = background/TimeMean; //Only valid if first column is off panel
-    
+
   for(int j = 0; j < meanSize; j++)
     {
       int row = j%scan_nypositions;
       int column = j/scan_nypositions;
-      
+
       double iMean = means[j];
       double iTime = times[j];
       double iMeanSubPE = iMean-(AvgBackgroundRate*iTime);
@@ -174,7 +194,7 @@ int analyzerV2_mean(const char* datafile, const char* timedata, bool PEConvert =
 
       meanHistSub->SetBinContent(column + 1, row + 1, iMeanSubPE);
     }
-    
+
   meanHistSub->SaveAs(Form("%s.root",Name));
 
   TCanvas *c1 = new TCanvas("c1","c1", 900, 588);
@@ -195,7 +215,7 @@ int analyzerV2_mean(const char* datafile, const char* timedata, bool PEConvert =
       meanHist->GetZaxis()->SetTitle("Pulse Min Value (V)");
     }
   meanHist->Draw("colz");
-  
+
   c2->cd();
   c2->SetRightMargin(0.15);
   meanHistSub->SetTitle(Form("Mean %d events - Background Subtracted",statistics));
@@ -209,7 +229,8 @@ int analyzerV2_mean(const char* datafile, const char* timedata, bool PEConvert =
       meanHistSub->GetZaxis()->SetTitle("Pulse Min Value (V)");
     }
   meanHistSub->Draw("colz");
-  
-  return 0;
-}
 
+  c1->Print("canvas1.png");
+  c2->Print("canvas2.png");
+
+}
