c001e39f4e934e70ccb862efbc84678b5eb733d1
Ron Belmont <belmonrj@gmail.com>
2015-08-21 16:15:57 -0600

    Added two new macros, DrawAsymmetry and DrawProjections

---
+++ DrawAsymmetry.C
@@ -0,0 +1,126 @@
+void DrawAsymmetry()
+{
+
+  // start("A1_LED_ColTests_Fiber1_VMIN_SIPM1_meanHistSub","A1_LED_ColTests_Fiber1_VMIN_SIPM2_meanHistSub");
+  // start("A1_LED_ColTests_Fiber2_VMIN_SIPM1_meanHistSub","A1_LED_ColTests_Fiber2_VMIN_SIPM2_meanHistSub");
+  // start("A1_LED_ColTests_Fiber3_VMIN_SIPM1_meanHistSub","A1_LED_ColTests_Fiber3_VMIN_SIPM2_meanHistSub");
+  // start("A1_LED_ColTests_Fiber4_VMIN_SIPM1_meanHistSub","A1_LED_ColTests_Fiber4_VMIN_SIPM2_meanHistSub");
+
+  start("A3_LED_Assymetry_VMin_SiPM1_meanHistSub","A3_LED_Assymetry_VMin_SiPM2_meanHistSub");
+
+  start("A3_LED_Assymetry_PannelBlock_VMin_SiPM1_meanHistSub","A3_LED_Assymetry_PannelBlock_VMin_SiPM2_meanHistSub");
+
+}
+
+
+
+void start(const char* name1, const char* name2)
+{
+
+  TFile *file1 = TFile::Open(Form("Data/ROOT/%s_projections.root",name1));
+  TFile *file2 = TFile::Open(Form("Data/ROOT/%s_projections.root",name2));
+
+  if(file1==NULL) {cout<<"cannot find file1"<<endl;return;}
+  if(file2==NULL) {cout<<"cannot find file2"<<endl;return;}
+
+  calc(file1,file2,name1,name2,0);
+  calc(file1,file2,name1,name2,1);
+  calc(file1,file2,name1,name2,2);
+  calc(file1,file2,name1,name2,3);
+  calc(file1,file2,name1,name2,4);
+  calc(file1,file2,name1,name2,5);
+  calc(file1,file2,name1,name2,6);
+  calc(file1,file2,name1,name2,7);
+  calc(file1,file2,name1,name2,8);
+
+}
+
+
+
+void calc(TFile *file1, TFile *file2, const char* name1, const char* name2, int projnumb)
+{
+
+  // generalize
+  TH1D *hp5_1 = (TH1D *)file1->Get(Form("%s_hproj_%d",name1,projnumb));
+  TH1D *hp5_2 = (TH1D *)file2->Get(Form("%s_hproj_%d",name2,projnumb));
+
+  if(hp5_1==NULL) {cout<<"cannot find hp5_1"<<endl; return;}
+  if(hp5_2==NULL) {cout<<"cannot find hp5_2"<<endl; return;}
+
+  int nbins = hp5_1->GetNbinsX();
+
+  // ----------------------------------------------------------------------------
+  // --- one of the preamps was broken, so we only had measurements from 1 SiPM
+  // --- I made this fake data part to help in the code development while waiting
+  // --- for new data with both SiPMs to be available
+  // ----------------------------------------------------------------------------
+  bool makefakedata = false;
+  if(makefakedata)
+    {
+      for(int i=0; i<nbins; i++)
+	{
+	  hp5_2->SetBinContent(i+1,hp5_1->GetBinContent(nbins-i));
+	}
+    }
+
+  // double max1 = hp5_1->GetBinContent(hp5_1->GetMaxBin());
+  // double max2 = hp5_2->GetBinContent(hp5_2->GetMaxBin());
+
+  double max1 = hp5_1->GetMaximum();
+  double max2 = hp5_2->GetMaximum();
+
+  hp5_1->GetXaxis()->SetTitle("distance (cm)");
+  hp5_2->GetXaxis()->SetTitle("distance (cm)");
+  hp5_1->GetYaxis()->SetTitle("Number of photoelectrons");
+  hp5_2->GetYaxis()->SetTitle("Number of photoelectrons");
+
+  if(max1>=max2)
+    {
+      // case 1
+      hp5_1->Draw();
+      hp5_2->Draw("same");
+    }
+  else
+    {
+      // case 2
+      hp5_2->Draw();
+      hp5_1->Draw("same");
+    }
+
+  // ---------
+
+  // need to adjust on case by case basis, unfortunately
+  char name3[200];
+  strcpy(name3,name1);
+  name3[22] = '\0'; // hackish way of truncating string
+
+  // generalize
+  c1->Print(Form("Figures/%s_together_p%d.png",name3,projnumb));
+  c1->Print(Form("Figures/%s_together_p%d.pdf",name3,projnumb));
+
+  TH1D *hp5_asymm = (TH1D *)hp5_1->Clone();
+  for(int i=0; i<nbins; i++)
+    {
+      double A = hp5_1->GetBinContent(i+1);
+      double B = hp5_2->GetBinContent(i+1);
+      double content = (B-A)/(B+A);
+      hp5_asymm->SetBinContent(i+1,content);
+      hp5_asymm->SetBinError(i+1,0);
+      //if(i<5||i>(nbins-6)) hp5_asymm->SetBinContent(i+1,-9); // skip the first 5 and last 5
+      //if(i<5||i>(nbins-6)) hp5_asymm->SetBinContent(i+1,-9); // skip the first 5 and last 5
+    }
+
+  hp5_asymm->Rebin(2);
+  hp5_asymm->Scale(0.5); // rebinning a th1 requires scaling
+  hp5_asymm->SetMarkerColor(kBlack);
+  hp5_asymm->SetMarkerStyle(kFullCircle);
+  hp5_asymm->Draw("ex0p");
+  hp5_asymm->GetYaxis()->SetTitle("Asymmetry of light yields");
+  hp5_asymm->SetMinimum(-1);
+  hp5_asymm->SetMaximum(1);
+
+  // generalize
+  c1->Print(Form("Figures/%s_asymmetry_p%d.png",name3,projnumb));
+  c1->Print(Form("Figures/%s_asymmetry_p%d.pdf",name3,projnumb));
+
+}
---
+++ DrawProjections.C
@@ -0,0 +1,109 @@
+void DrawProjections()
+{
+
+  // drawproj("A1_LED_ColTests_Fiber1_VMIN_SIPM1_meanHistSub");
+  // drawproj("A1_LED_ColTests_Fiber2_VMIN_SIPM1_meanHistSub");
+  // drawproj("A1_LED_ColTests_Fiber3_VMIN_SIPM1_meanHistSub");
+  // drawproj("A1_LED_ColTests_Fiber4_VMIN_SIPM1_meanHistSub");
+
+  drawproj("A3_LED_Assymetry_VMin_SiPM1_meanHistSub");
+  drawproj("A3_LED_Assymetry_VMin_SiPM1_meanHistSub");
+
+  drawproj("A3_LED_Assymetry_PannelBlock_VMin_SiPM1_meanHistSub");
+  drawproj("A3_LED_Assymetry_PannelBlock_VMin_SiPM1_meanHistSub");
+
+}
+
+void drawproj(const char* basename)
+{
+
+  TFile *file = TFile::Open(Form("Data/ROOT/%s_projections.root",basename));
+
+  TH1D *hp0 = (TH1D*)file->Get(Form("%s_hproj_0",basename));
+  TH1D *hp1 = (TH1D*)file->Get(Form("%s_hproj_1",basename));
+  TH1D *hp2 = (TH1D*)file->Get(Form("%s_hproj_2",basename));
+  TH1D *hp3 = (TH1D*)file->Get(Form("%s_hproj_3",basename));
+  TH1D *hp4 = (TH1D*)file->Get(Form("%s_hproj_4",basename));
+  TH1D *hp5 = (TH1D*)file->Get(Form("%s_hproj_5",basename));
+  TH1D *hp6 = (TH1D*)file->Get(Form("%s_hproj_6",basename));
+  TH1D *hp7 = (TH1D*)file->Get(Form("%s_hproj_7",basename));
+  TH1D *hp8 = (TH1D*)file->Get(Form("%s_hproj_8",basename));
+
+  hp5->SetLineColor(kBlack);
+  hp5->SetLineWidth(2);
+  hp5->Draw(); // directly over fiber (THIS MAY CHANGE FROM SCAN SCAN TO SCAN, NEED TO BE CAREFUL)
+  hp5->SetMinimum(0.0);
+  hp5->GetXaxis()->SetTitle("distance (cm)");
+  hp5->GetYaxis()->SetTitle("Number of photoelectrons");
+  //hp5->GetYaxis()->SetTitle("Estimated number of photoelectrons");
+  // hp4->Add(hp6);
+  // hp4->Scale(0.5); // average 4 and 6
+  hp4->SetLineColor(kRed);
+  hp4->SetLineWidth(2);
+  hp4->Draw("same"); // 0.5 cm away from fiber
+  hp3->Add(hp7);
+  hp3->Scale(0.5); // average 3 and 7
+  hp3->SetLineColor(kBlue);
+  hp3->SetLineWidth(2);
+  hp3->Draw("same"); // 1.0 cm away from fiber
+  hp2->Add(hp8);
+  hp2->Scale(0.5); // average 2 and 8
+  hp2->SetLineColor(kGreen+2);
+  hp2->SetLineWidth(2);
+  hp2->Draw("same"); // 1.5 cm away from fiber
+  hp1->Add(hp0);
+  hp1->Scale(0.5); // average 0 (2.5 cm away) and 1 (2.0 cm away)
+  hp1->SetLineColor(kMagenta);
+  hp1->SetLineWidth(2);
+  // hp1->Draw("same"); // 2.25 cm away from fiber, doesn't look nice
+
+  TLegend *leg = new TLegend(0.56,0.68,0.88,0.88);
+  leg->AddEntry(hp5,"over fiber","l");
+  leg->AddEntry(hp4,"0.5 cm off fiber","l");
+  leg->AddEntry(hp3,"1.0 cm off fiber","l");
+  leg->AddEntry(hp2,"1.5 cm off fiber","l");
+  leg->SetTextSize(0.05);
+  leg->Draw();
+
+  c1->Print(Form("Figures/%s_projections.png",basename));
+  c1->Print(Form("Figures/%s_projections.pdf",basename));
+
+  double Ihp1 = hp1->Integral();
+  double Ihp2 = hp2->Integral();
+  double Ihp3 = hp3->Integral();
+  double Ihp4 = hp4->Integral();
+  double Ihp5 = hp5->Integral();
+
+
+  hp5->Scale(1/Ihp5);
+  hp4->Scale(1/Ihp4);
+  hp3->Scale(1/Ihp3);
+  hp2->Scale(1/Ihp2);
+  hp1->Scale(1/Ihp1);
+  hp5->Draw();
+  hp5->GetYaxis()->SetTitleOffset(1.3);
+  hp2->Draw("same");
+  hp3->Draw("same");
+  hp4->Draw("same");
+
+  TLegend *leg2 = new TLegend(0.33,0.68,0.88,0.88);
+  leg2->AddEntry(hp5,Form("over fiber, #times %f",1/Ihp1),"l");
+  leg2->AddEntry(hp4,Form("0.5 cm off fiber, #times %f",1/Ihp2),"l");
+  leg2->AddEntry(hp3,Form("1.0 cm off fiber, #times %f",1/Ihp3),"l");
+  leg2->AddEntry(hp2,Form("1.5 cm off fiber, #times %f",1/Ihp4),"l");
+  leg2->SetTextSize(0.05);
+  //leg2->Draw();
+
+  TLegend *leg3 = new TLegend(0.43,0.63,0.88,0.88);
+  leg3->AddEntry(hp5,"over fiber","l");
+  leg3->AddEntry(hp4,"0.5 cm off fiber","l");
+  leg3->AddEntry(hp3,"1.0 cm off fiber","l");
+  leg3->AddEntry(hp2,"1.5 cm off fiber","l");
+  leg3->AddEntry((TObject*)0x0,"Normalized to integral","");
+  leg3->SetTextSize(0.05);
+  leg3->Draw();
+
+  c1->Print(Form("Figures/%s_projectionsSC.png",basename));
+  c1->Print(Form("Figures/%s_projectionsSC.pdf",basename));
+
+}
