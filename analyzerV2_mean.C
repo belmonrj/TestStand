cc25ec73c55857cbd29a9b8b24a91b4eb030ee67
Ron Belmont <belmonrj@gmail.com>
2015-09-11 11:17:19 -0600

    Added lots of comments to analyzer

--- analyzerV2_mean.C
+++ analyzerV2_mean.C
@@ -40,7 +40,8 @@ void analyzerV2_mean()
   doana("20150901-1748_A1_LED");
   doana("20150902-1005_A1_LED");
   doana("20150909-1318_A1_LED");
-  doana("20150910-1215_A1_LED"); // looks bad, triggered on coincidence instead of function generator
+  doana("20150910-1215_A1_LED"); // looks bad, problem with background subtraction (alignment)
+  doana("20150910-1736_A1_Source"); // looks bad, problem with background subtraction (alignment)
 
 }
 
@@ -63,146 +64,130 @@ void doana(const char *basename)
 void analyze(const char* NAME, const char* timedata, bool PEConvert, double PE)
 {
 
-  ////////////////////////////////////////////////////////////////////////////////
-  //MODIFY FOR SIZE OF PANEL. CHECK TOTAL FILES MANUALLY FOR COMPATIBILITY HERE.//
-  ////////////////////////////////////////////////////////////////////////////////
+  // ----------------------------------------------------------------------------------- //
+  // --- MODIFY FOR SIZE OF PANEL, CHECK TOTAL FILES MANUALLY FOR COMPATIBILITY HERE --- //
+  // ----------------------------------------------------------------------------------- //
 
-  int statistics = 1500; //Total pulse events per location
-
-  // CHECK THESE WHEN USING
+  // --- CHECK THESE WHEN USING
   int scan_nxpositions = 58;
   int scan_nypositions = 9;
 
-  // ADJUST AS NEEDED
-  //double PE = 0.004386; //Measured value of PE peak spacing. -- // this is SiPM 1
-
   int totalBins = scan_nxpositions * scan_nypositions;
 
-  double background = 0.0; //Defining for noise subtraction
+  double background = 0.0; // used for noise subtraction
 
-  ////////////////////////////////////////////
-  /////////////getting event data/////////////
-  ////////////////////////////////////////////
 
+
+  // ------------------------------
+  // --- now read in the event data
+  // ------------------------------
+
+
+  // ----------------------------
+  // --- read in the voltage data
   int numb = 0;
   vector <double> means;
   string line;
   ifstream file;
   ifstream file_cnt;
-
+  // ----------------------------
   file.open(Form("Data/Text/%s.txt",NAME));
-
   for(int i = 0; i < totalBins ; i++)
     {
-      // string meanval_s;
-      // getline(file,meanval_s);
-      // double meanval_d = std::stod(meanval_s);
-      // means.push_back(-1*meanval_d);
       double meanval_d = -9999;
       file>>meanval_d;
       if ( PEConvert ) means.push_back(-1*meanval_d);
       else means.push_back(meanval_d);
       if(i==0) cout<<meanval_d<<endl;
     }
-
   file.close();
-
   int meanSize = means.size();
 
-  /////////////////////////////////////////////////////////////////////////////
-  //////////////////// Getting Average background Rate ////////////////////////
-  /////////////////////////////////////////////////////////////////////////////
 
+  // -------------------------
+  // --- read in the time data
   int timenumb = 0;
   vector <double> times;
   string timeline;
   ifstream timefile;
-
+  // -------------------------
   timefile.open(Form("Data/Text/%s",timedata));
-  //cout<<Form("Data/Text/%s",timedata)<<endl;;
-
-  for(int i = 0; i < totalBins ; i++)  //Gets time data
+  for(int i = 0; i < totalBins ; i++)
     {
-      // string timeval_s;
-      // getline(timefile,timeval_s);
-      // double timeval_d = std::stod(timeval_s);
-      // times.push_back(timeval_d);
       double timeval_d = -9999;
       timefile>>timeval_d;
       times.push_back(timeval_d);
       if(i==0) cout<<timeval_d<<endl;
     }
-
   timefile.close();
-
   int timeSize = times.size();
 
-  double TimeSum = 0;
-
+  double TimeSum = 0;  // get total time for first column
   for ( int i=0; i<timeSize; i++ )
-    {  //Gets total time for first column
-
+    {
       int row = i%scan_nypositions;
       int column = i/scan_nypositions;
-
       if (column == 1) TimeSum += times[i];
     }
-
   double TimeMean = TimeSum/scan_nypositions;
 
-  ////////////////////////////////////////////////////////////
-  //char NAME[] = "meanHistSub"; //Name the saved histogram HERE
-  ////////////////////////////////////////////////////////////
 
-  double distanceX = (double)scan_nxpositions/2; //For 2 steps per cm. Check Stepper code for agreement
+  // ---
+
+  // --- determine the distance based on the number of steps
+  // --- default is 2 steps per cm, double check stepper code
+  double distanceX = (double)scan_nxpositions/2;
   double distanceY = (double)scan_nypositions/2;
-
+  // --- make the histogram for the unsubtracted
   TH2D *meanHist = new TH2D(Form("%s_meanHist",NAME), Form("%s_meanHist",NAME),
-			    scan_nxpositions,0.0,distanceX, scan_nypositions,0.0,distanceY); // Modify numbers for cm scale on panel.
-
+			    scan_nxpositions,0.0,distanceX, scan_nypositions,0.0,distanceY);
+  // --- make the histogram for the background subtracted
   TH2D *meanHistSub = new TH2D(Form("%s_meanHistSub",NAME), Form("%s_meanHistSub",NAME),
-			       scan_nxpositions,0.0,distanceX, scan_nypositions,0.0,distanceY); // Modify numbers for cm scale on panel.
+			       scan_nxpositions,0.0,distanceX, scan_nypositions,0.0,distanceY);
 
   for(int i = 0; i < meanSize; i++)
     {
-
       int row = i%scan_nypositions;
       int column = i/scan_nypositions;
       double iMean = means[i];
-
+      // --- THIS IS A HUGE PROBLEM
       if(column == 1)
 	{
-	  background += iMean;  // Gets total event data for first column
+	  background += iMean;
 	}
-
-      meanHist->SetBinContent(column+1,row+1,iMean);  //Fills unsubtracted histogram
+      // --- fill the histogram without the background subtraction
+      meanHist->SetBinContent(column+1,row+1,iMean);
       if (PEConvert == true)
 	{
 	  meanHist->SetBinContent(column+1,row+1,iMean/PE);
 	}
     }
 
+  // --- calculate the background
   background = background/scan_nypositions;
-  double AvgBackgroundRate = background/TimeMean; //Only valid if first column is off panel
-
+  double AvgBackgroundRate = background/TimeMean; // only valid if first column is off panel // this is a huge problem
   for(int j = 0; j < meanSize; j++)
     {
       int row = j%scan_nypositions;
       int column = j/scan_nypositions;
-
       double iMean = means[j];
       double iTime = times[j];
       double iMeanSubPE = iMean-(AvgBackgroundRate*iTime);
-
       if (PEConvert == true)
 	{
 	  iMeanSubPE = (iMean-(AvgBackgroundRate*iTime))/PE;
 	}
-
+      // --- fill the histogram with the background subtraction
       meanHistSub->SetBinContent(column + 1, row + 1, iMeanSubPE);
     }
-
+  // --- write the background subtracted histogram to a ROOT file
   meanHistSub->SaveAs(Form("Data/ROOT/%s_meanHistSub.root",NAME)); // NEEDS TO BE REVISED
+
+
+
+  // ---------------------------------------------- //
+  // --- now make figures of the 2d panel plots --- //
+  // ---------------------------------------------- //
 
   TCanvas *c1 = new TCanvas("c1","c1", 900, 588);
   TCanvas *c2 = new TCanvas("c2","c2", 900, 588);
@@ -211,7 +196,6 @@ void analyze(const char* NAME, const char* timedata, bool PEConvert, double PE)
 
   c1->cd();
   c1->SetRightMargin(0.15);
-  meanHist->SetTitle(Form("Mean %d events",statistics));
   meanHist->GetXaxis()->SetTitle("Position Y (cm)");
   meanHist->GetYaxis()->SetTitle("Position X (cm)");
   if (PEConvert == true) meanHist->GetZaxis()->SetTitle("Pulse Min Value (photoelectrons)");
@@ -220,7 +204,6 @@ void analyze(const char* NAME, const char* timedata, bool PEConvert, double PE)
 
   c2->cd();
   c2->SetRightMargin(0.15);
-  meanHistSub->SetTitle(Form("Mean %d events - Background Subtracted",statistics));
   meanHistSub->GetXaxis()->SetTitle("Position Y (cm)");
   meanHistSub->GetYaxis()->SetTitle("Position X (cm)");
   if (PEConvert == true) meanHistSub->GetZaxis()->SetTitle("Pulse Min Value (photoelectrons)");
