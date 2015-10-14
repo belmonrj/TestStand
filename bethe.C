24da443b374164fb38c22e4aa64cead150b1303d
Ron Belmont <belmonrj@gmail.com>
2015-10-14 15:49:34 -0600

    Added bethe.C, a simple macro for calculating dEdx based on energy and particle mass

---
+++ bethe.C
@@ -0,0 +1,74 @@
+const double mu_mass = 106; // MeV, 105.6583715...
+const double e_mass = 0.511; // MeV, 0.510998910...
+const double density = 1.060; // g/cm^3
+const double e_radius = 2.81794; // fm
+const double pi = 3.1415926535; // pi
+const double N_A = 6.0221415e23; // Avogadro's number
+
+
+
+void bethe()
+{
+
+  dobethe(318,mu_mass);
+  dobethe(367,mu_mass); // guessed min by process of elimination
+
+}
+
+
+
+double dobethe(double energy = 318, double mass = mu_mass)
+{
+
+  double gamma = energy/mass;
+  double beta = calcbeta(gamma);
+
+  //cout << gamma << " " << beta << " " << beta*gamma << endl;
+
+
+  double answer = -9999;
+
+  double ZoverA = 0.53768;
+  double A = 11.1592; // average atomic weight
+  double K = 0.307075; // K is some constant relating to electron density, MeV/g/cm^2
+  double I = 68.7; // mean excitation energy, in eV
+  I /= 1.0e6; // covert to MeV;
+  double z = 1; // 1 for electrons, muons, protons, etc... 2 for alpha particles
+
+
+  double r_e = e_radius/1.0e13;
+  double calcK = 4*pi*N_A*r_e*r_e*e_mass;
+
+  //cout << "K is " << K << endl;
+  //cout << "calcK is " << calcK << endl;
+
+  double firstblock = (K*z*z*ZoverA)/(beta*beta);
+  double Tmax (2*e_mass*beta*beta*gamma*gamma)/(1 + (2*gamma*e_mass/mass) + (e_mass/mass)*(e_mass/mass));
+  double logargument = (2*e_mass*beta*beta*gamma*gamma*Tmax)/(I*I);
+
+  //cout << "log argument is " << logargument << endl;
+  //cout << "first block is  " << firstblock << endl;
+
+  answer = firstblock * (0.5 * log(logargument) - (beta*beta));
+
+  answer *= density;
+
+  cout << "For gamma = " << gamma << " <dE/dx> = " << answer << endl;
+
+  return answer;
+
+}
+
+
+double calcbeta(double gamma)
+{
+  // gamma = 1/sqrt(1-beta^2)
+  double x = 1.0/(gamma*gamma); // = 1 - beta^2
+  x *= -1; // beta^2 - 1;
+  x += 1; // beta^2
+  if(x>0) return(sqrt(x));
+  else
+    {
+      return -9999;
+    }
+}
