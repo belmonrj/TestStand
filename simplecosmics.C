4df44335c4f7397e9b3183225996a262febdbb70
Ron Belmont <belmonrj@gmail.com>
2015-10-17 12:53:59 -0600

    Massive clean up of old unwanted stuff and change from average to sum simple cosmics

--- simplecosmics.C
+++ simplecosmics.C
@@ -5,7 +5,7 @@ double peconvert = 0.00502; // volts per photoelectrion
 void simplecosmics()
 {
 
-  doit("20150930-1720");
+  //doit("20150930-1720");
   doit("20151009-1743");
 
 }
@@ -93,25 +93,11 @@ void doit(const char *basename)
 
   // --- make a canvas and draw the histogram
   TCanvas *c1 = new TCanvas("c1","",800,600);
-  h1->Draw();
-  // c1->Print(Form("Cosmics/%s_uglydata.png",basename));
-  c1->SetLogy();
-  // c1->Print(Form("Cosmics/%s_uglydatalog.png",basename));
-  c1->SetLogy(0);
-  // h1->SetMaximum(120);
-  // c1->Print(Form("Cosmics/%s_uglydatanotlog.png",basename));
 
   // --- rescale the histograms from volts to photoelectrons
   h1->GetXaxis()->SetLimits(newmin/peconvert,newmax/peconvert);
   h1->GetXaxis()->SetTitle("Number of photoelectrons");
   h1->GetYaxis()->SetTitle("Counts");
-  h1->Draw();
-  // c1->Print(Form("Cosmics/%s_uglydata.png",basename));
-  c1->SetLogy();
-  // c1->Print(Form("Cosmics/%s_uglydatalog.png",basename));
-  c1->SetLogy(0);
-  h1->SetMaximum(210);
-  // c1->Print(Form("Cosmics/%s_uglydatanotlog.png",basename));
 
   // --- define Landau function and draw
   // --- don't fit yet because the data have tons of ugly low voltage1 background
@@ -122,129 +108,10 @@ void doit(const char *basename)
   fun->SetParameter(0,height);
   fun->SetParameter(1,mu);
   fun->SetParameter(2,sigma);
-  fun->Draw("same");
-  fun->SetLineColor(kGreen+2);
-  fun->SetLineWidth(1);
-  c1->Print(Form("Cosmics/%s_uglydatanotlogfit.png",basename));
-  c1->Print(Form("Cosmics/%s_uglydatanotlogfit.pdf",basename));
-  c1->SetLogy(1);
-  c1->Print(Form("Cosmics/%s_uglydatalogfit.png",basename));
-  c1->Print(Form("Cosmics/%s_uglydatalogfit.pdf",basename));
 
-  // --- now draw SiPM2 on top
-  h1->SetLineColor(kRed);
-  h2->SetLineColor(kBlue);
-  h2->GetXaxis()->SetLimits(newmin/peconvert,newmax/peconvert);
-  h2->Draw("same");
-  c1->SetLogy(0);
-  c1->Print(Form("Cosmics/%s_uglydatabothnotlogfit.png",basename));
-  c1->Print(Form("Cosmics/%s_uglydatabothnotlogfit.pdf",basename));
-  c1->SetLogy(1);
-  c1->Print(Form("Cosmics/%s_uglydatabothlogfit.png",basename));
-  c1->Print(Form("Cosmics/%s_uglydatabothlogfit.pdf",basename));
-
-  h1->SetMaximum(1.15*h1->GetBinContent(h1->GetMaximumBin()));
-
-  c1->SetLogy(0);
-  c1->Print(Form("Cosmics/%s_fuglydatabothnotlogfit.png",basename));
-  c1->Print(Form("Cosmics/%s_fuglydatabothnotlogfit.pdf",basename));
-  c1->SetLogy(1);
-  c1->Print(Form("Cosmics/%s_fuglydatabothlogfit.png",basename));
-  c1->Print(Form("Cosmics/%s_fuglydatabothlogfit.pdf",basename));
 
   double bgscale = 650; // guess...
-  TF1 *bun = new TF1("bun","[0]*TMath::Exp([1]*x) + [2]*TMath::Exp([3]*x)",newmin/peconvert,newmax/peconvert);
-  bun->SetParameter(0,bgscale*7.17020);
-  bun->SetParameter(1,-5.10223e-1);
-  bun->SetParameter(2,bgscale*1.22363e-1);
-  bun->SetParameter(3,-8.84094e-2);
-  bun->SetLineColor(kGreen+2);
-  bun->SetLineWidth(1);
-  bun->Draw("same");
 
-  c1->SetLogy(0);
-  c1->Print(Form("Cosmics/%s_buglydatabothnotlogfit.png",basename));
-  c1->Print(Form("Cosmics/%s_buglydatabothnotlogfit.pdf",basename));
-  c1->SetLogy(1);
-  c1->Print(Form("Cosmics/%s_buglydatabothlogfit.png",basename));
-  c1->Print(Form("Cosmics/%s_buglydatabothlogfit.pdf",basename));
-
-
-
-  TF1 *ultrafun = new TF1("ultrafun","[0]*TMath::Exp([1]*x) + [2]*TMath::Exp([3]*x) + [4]*TMath::Landau(x,[5],[6])",newmin/peconvert,newmax/peconvert);
-  ultrafun->SetParameter(0,bgscale*7.17020);
-  ultrafun->SetParameter(1,-5.10223e-1);
-  ultrafun->SetParameter(2,bgscale*1.22363e-1);
-  ultrafun->SetParameter(3,-8.84094e-2);
-  ultrafun->SetParameter(4,height);
-  ultrafun->SetParameter(5,mu);
-  ultrafun->SetParameter(6,sigma);
-  ultrafun->SetLineColor(kBlack);
-  ultrafun->Draw("same");
-
-  TLegend *leg = new TLegend(0.48,0.68,0.88,0.88);
-  leg->AddEntry(h1,"SiPM1","l");
-  leg->AddEntry(h2,"SiPM2","l");
-  leg->AddEntry(fun,"Signal (Landau)","l");
-  leg->AddEntry(bun,"Background (double expo)","l");
-  leg->AddEntry(ultrafun,"Combined functional fit","l");
-  leg->Draw();
-
-  c1->SetLogy(0);
-  c1->Print(Form("Cosmics/%s_bhuglydatabothnotlogfit.png",basename));
-  c1->Print(Form("Cosmics/%s_bhuglydatabothnotlogfit.pdf",basename));
-  c1->SetLogy(1);
-  c1->Print(Form("Cosmics/%s_bhuglydatabothlogfit.png",basename));
-  c1->Print(Form("Cosmics/%s_bhuglydatabothlogfit.pdf",basename));
-
-  h1->Fit(ultrafun,"R");
-  bun->SetParameter(0,ultrafun->GetParameter(0));
-  bun->SetParameter(1,ultrafun->GetParameter(1));
-  bun->SetParameter(2,ultrafun->GetParameter(2));
-  bun->SetParameter(3,ultrafun->GetParameter(3));
-  fun->SetParameter(0,ultrafun->GetParameter(4));
-  fun->SetParameter(1,ultrafun->GetParameter(5));
-  fun->SetParameter(2,ultrafun->GetParameter(6));
-  c1->SetLogy(0);
-  c1->Print(Form("Cosmics/%s_bhfuglydatabothnotlogfit.png",basename));
-  c1->Print(Form("Cosmics/%s_bhfuglydatabothnotlogfit.pdf",basename));
-  c1->SetLogy(1);
-  c1->Print(Form("Cosmics/%s_bhfuglydatabothlogfit.png",basename));
-  c1->Print(Form("Cosmics/%s_bhfuglydatabothlogfit.pdf",basename));
-
-  h1->SetMaximum(210);
-  c1->SetLogy(0);
-  c1->Print(Form("Cosmics/%s_pfuglydatabothnotlogfit.png",basename));
-  c1->Print(Form("Cosmics/%s_pfuglydatabothnotlogfit.pdf",basename));
-  c1->SetLogy(1);
-  c1->Print(Form("Cosmics/%s_pfuglydatabothlogfit.png",basename));
-  c1->Print(Form("Cosmics/%s_pfuglydatabothlogfit.pdf",basename));
-
-
-
-  // ---
-
-  h1->SetMaximum(1.15*h1->GetBinContent(h1->GetMaximumBin()));
-  h1->Draw();
-  h2->Draw("same");
-
-  TF1 *pain = new TF1("pain","[0]*7.17020*TMath::Exp([1]*x) + [0]*1.22363*TMath::Exp([2]*x) + [3]*TMath::Landau(x,[4],[5])",newmin/peconvert,newmax/peconvert);
-  pain->SetParameter(0,bgscale);
-  pain->FixParameter(1,-5.10223e-1);
-  pain->FixParameter(2,-8.84094e-2);
-  pain->SetParameter(3,height);
-  pain->SetParameter(4,mu);
-  pain->SetParameter(5,sigma);
-  pain->SetLineColor(kBlack);
-  pain->Draw("same");
-
-  h1->Fit(pain,"R");
-  c1->SetLogy(0);
-  c1->Print(Form("Cosmics/%s_iampain.png",basename));
-  c1->Print(Form("Cosmics/%s_iampain.pdf",basename));
-  c1->SetLogy(1);
-  c1->Print(Form("Cosmics/%s_iampain_log.png",basename));
-  c1->Print(Form("Cosmics/%s_iampain_log.pdf",basename));
 
 
 
@@ -340,8 +207,8 @@ void doit(const char *basename)
 
 
 
-  hsum->GetXaxis()->SetLimits(newmin/peconvert,newmax/peconvert);
-  hsum->GetXaxis()->SetTitle("Number of photoelectrons (SiPM1+SiPM2)/2");
+  hsum->GetXaxis()->SetLimits(2*newmin/peconvert,2*newmax/peconvert);
+  hsum->GetXaxis()->SetTitle("Number of photoelectrons SiPM1+SiPM2");
   hsum->Draw();
   c1->Print(Form("Cosmics/%s_temp.png",basename));
   c1->Print(Form("Cosmics/%s_temp.pdf",basename));
@@ -355,19 +222,8 @@ void doit(const char *basename)
 
 
   bgscale = 100;
-  TF1 *simplefun = new TF1("simplefun","[0]*TMath::Exp([1]*x) + [2]*TMath::Landau(x,[3],[4])",newmin/peconvert,newmax/peconvert);
-  simplefun->SetParameter(0,bgscale*7.17020);
-  simplefun->SetParameter(1,-5.10223e-1);
-  simplefun->SetParameter(2,height);
-  simplefun->SetParameter(3,mu);
-  simplefun->SetParameter(4,sigma);
-  simplefun->SetLineColor(kBlack);
-  c1->Clear();
-  //simplefun->Draw("same");
-  //hsum->Fit(simplefun,"","",0,120);
-  //hsum->Fit(ultrafun,"","",0,120);
-  //hsum->Fit("landau","","",15,120);
-  hsum->Fit(fun,"","",20,60);
+  //hsum->Fit(fun,"","",20,60);
+  hsum->Fit(fun,"","",40,120);
   fun->SetLineColor(kRed);
   fun->SetLineWidth(2);
   fun->Draw("same");
@@ -396,13 +252,14 @@ void doit(const char *basename)
   //fun->Draw("same");
   //hsum->Fit(simplefun,"","",0,60);
 
-  TF1 *fun2 = new TF1("fun2","([0]/sqrt(6.28))*TMath::Exp(-0.5*((x-[1])/[2] + TMath::Exp(-(x-[1])/[2])))",0,120);
+  TF1 *fun2 = new TF1("fun2","([0]/sqrt(6.28))*TMath::Exp(-0.5*((x-[1])/[2] + TMath::Exp(-(x-[1])/[2])))",0,250);
   fun2->SetParameter(0,hax);
   fun2->SetParameter(1,mux);
   fun2->SetParameter(2,six);
   //fun2->SetLineColor(kBlack);
 
-  hsum->Fit(fun2,"","",15,60);
+  //hsum->Fit(fun2,"","",15,60);
+  hsum->Fit(fun2,"","",30,120);
   fun2->Draw("same");
 
   cout << fun2->GetChisquare() << "/" << fun2->GetNDF() << endl;
