389689717ba3b1bf10d07e8f7439899676010537
Ron Belmont <belmonrj@gmail.com>
2015-10-19 17:04:11 -0600

    Added beta spectrum overlay to source

--- source.C
+++ source.C
@@ -308,4 +308,78 @@ void source()
   c1->SetLogy(1);
   c1->Print(Form("Source/source_lskewgengaus_sumlog.png"));
   c1->Print(Form("Source/source_lskewgengaus_sumlog.pdf"));
+
+
+  // --- way too much fun is about to be had...
+  TF1 *fun_Sr = new TF1("fun_Sr","(x<0.546)*([0]*x-[0]*0.546) + (x>0.546)*0",0,2.5);
+  TF1 *fun_Y = new TF1("fun_Y","(x<2.28)*([0]*x-[0]*2.28) + (x>2.28)*0",0,2.5);
+  double slope_Sr = -14/0.546;
+  double slope_Y = -8/2.28;
+  fun_Sr->SetParameter(0,slope_Sr);
+  fun_Y->SetParameter(0,slope_Y);
+
+  // --- Kurie
+  TH1D *h_Sr = new TH1D("h_Sr","",100,0.0,2.5);
+  TH1D *h_Y = new TH1D("h_Y","",100,0.0,2.5);
+  // --- Kurie squared
+  TH1D *h2_Sr = new TH1D("h2_Sr","",100,0.0,2.5);
+  TH1D *h2_Y = new TH1D("h2_Y","",100,0.0,2.5);
+  // --- Kurie squared times momentum squared
+  TH1D *h2p_Sr = new TH1D("h2p_Sr","",100,0.0,2.5);
+  TH1D *h2p_Y = new TH1D("h2p_Y","",100,0.0,2.5);
+  // --- Kurie squared times momentum squared times Fermi function = number
+  TH1D *h2pF_Sr = new TH1D("h2pF_Sr","",100,0.0,2.5);
+  TH1D *h2pF_Y = new TH1D("h2pF_Y","",100,0.0,2.5);
+  for(int i=0; i<100; i++)
+    {
+      double x = h_Sr->GetBinCenter(i+1);
+      double spec_Sr = fun_Sr->Eval(x);
+      double spec_Y = fun_Y->Eval(x);
+      h_Sr->SetBinContent(i+1,spec_Sr);
+      h_Y->SetBinContent(i+1,spec_Y);
+      double sspec_Sr = spec_Sr**2;
+      double sspec_Y = spec_Y**2;
+      h2_Sr->SetBinContent(i+1,sspec_Sr);
+      h2_Y->SetBinContent(i+1,sspec_Y);
+      double m = 0.511;
+      double E = x+m;
+      double p2 = E*E - m*m; // momentum squared
+      double p = sqrt(p2); // momentum
+      h2p_Sr->SetBinContent(i+1,p2*sspec_Sr);
+      h2p_Y->SetBinContent(i+1,p2*sspec_Y);
+      // ---
+      double pi = 3.14159265;
+      double alpha = 1.0/137.0; // fine structure constant
+      double Z = 38; // atomic number
+      double eta = -alpha*Z*E/p; // variable for fermi function
+      double F_N = 2*pi*eta/(1-exp(-2*pi*eta)); // non relativistic Fermi function
+      double gamma = E/m;
+      double S = sqrt(1-alpha*alpha*Z*Z)-1; // variable for relativistic fermi function
+      double relcorr = pow(E*E*(1+4*gamma*gamma)-1,S); // relativist correction
+      double F = F_N*relcorr; // relativistic Fermi function
+      h2pF_Sr->SetBinContent(i+1,F*p2*sspec_Sr);
+      h2pF_Y->SetBinContent(i+1,F*p2*sspec_Y);
+    }
+
+  c1->SetLogy(0);
+  TH1D *h2pF_sum = (TH1D*)h2pF_Sr->Clone();
+  h2pF_sum->Add(h2pF_Y);
+  h2pF_sum->Scale(100);
+  hsum->GetFunction("skewgengaus")->SetBit(TF1::kNotDraw);
+  hsum->Draw();
+  //h2pF_sum->GetXaxis()->SetLimits(0,75); // default 0,2.5...
+  h2pF_sum->SetLineColor(kRed);
+  h2pF_sum->SetLineWidth(2);
+  h2pF_sum->GetXaxis()->SetLimits(20,105); // default 0,2.5...
+  h2pF_sum->Draw("same");
+  c1->Print("monday.png");
+
+  h2pF_sum->GetXaxis()->SetLimits(0,75); // default 0,2.5...
+  h2pF_sum->Draw("same");
+  c1->Print("monday2.png");
+
+  h2pF_sum->GetXaxis()->SetLimits(0,120); // default 0,2.5...
+  h2pF_sum->Draw("same");
+  c1->Print("monday3.png");
+
 }
