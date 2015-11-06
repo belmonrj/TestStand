651eb01ccfa6db6b01cc6ac9838ee5d4acb8b626
Ron Belmont <belmonrj@gmail.com>
2015-11-05 17:34:19 -0700

    Added two new macros for the new LED scans, yslice_new_... needs SiPM2 stuff added

---
+++ yslice_new_LEDscan.C
@@ -0,0 +1,71 @@
+void yslice_new_LEDscan()
+{
+
+  doyslice("20151105-1515");
+
+}
+
+
+void doyslice(const char *basename)
+{
+
+  char name[200];
+  sprintf(name,"%s_VMIN_SIPM1_meanHistSub",basename);
+  ySlice(name);
+
+}
+
+
+void ySlice(const char *NAME)
+{
+
+  cout<<"NAME is "<<NAME<<endl;
+
+  TFile* inFile = TFile::Open(Form("Data/NewLED/%s.root",NAME));
+  if(inFile==NULL)
+    {
+      cout<<"cannot find file, exiting"<<endl;
+      return;
+    }
+  //inFile->ls();
+
+  //TH2D *h1 = (TH2D*)inFile->Get(Form("meanHistSub"));
+  TH2D *h1 = (TH2D*)inFile->Get(Form("%s",NAME));
+  if(h1==NULL)
+    {
+      cout<<"no histogram available, exiting"<<endl;
+      return;
+    }
+
+  TFile *fout = new TFile(Form("Data/NewLED/%s_projections.root",NAME),"recreate");
+
+  int nybins = h1->GetNbinsY();
+  for(int i=0; i<nybins; i++)
+    {
+      cout<<"in loop "<<i<<" NAME is "<<NAME<<endl;
+      TH1D *hpx = (TH1D *)h1->ProjectionX(Form("hprojX_%d",NAME,i),i+1,i+1,"");
+      //hpx->SetTitle(Form("%s ProjectionX %d",NAME,i));
+      //hpx->SetTitle("");
+      hpx->Draw();
+      c1->Print(Form("Figures/Burn/%s_projectionX_%d.png",NAME,i));
+      c1->Print(Form("Figures/Burn/%s_projectionX_%d.pdf",NAME,i));
+    }
+
+  int nxbins = h1->GetNbinsX();
+  for(int i=0; i<nxbins; i++)
+    {
+      cout<<"in loop "<<i<<" NAME is "<<NAME<<endl;
+      TH1D *hpy = (TH1D *)h1->ProjectionY(Form("hprojY_%d",NAME,i),i+1,i+1,"");
+      //hpy->SetTitle(Form("%s ProjectionY %d",NAME,i));
+      //hpy->SetTitle("");
+      hpy->Draw();
+      c1->Print(Form("Figures/Burn/%s_projectionY_%d.png",NAME,i));
+      c1->Print(Form("Figures/Burn/%s_projectionY_%d.pdf",NAME,i));
+    }
+
+  fout->Write();
+  fout->Close();
+
+  delete fout;
+
+}
