ff051f01942bb465dc310d573b38b14eb042dfe5
Ron Belmont <belmonrj@gmail.com>
2016-03-24 17:19:14 -0600

    Added gumbel function fit

--- SimpleCosmics.C
+++ SimpleCosmics.C
@@ -5,13 +5,24 @@ void ReadSingleFile(const char*, const char*);
 void SimpleCosmics()
 {
 
-  ReadSingleFile("Data/20160315-2359_Unaveraged_VMin1.txt","OH-1-3");
-  ReadSingleFile("Data/20160315-2359_Unaveraged_VMin2.txt","OH-2-6");
+  ReadSingleFile("Data/20160314-1215_Unaveraged_VMin1.txt","OH-2-46");
+  ReadSingleFile("Data/20160314-1215_Unaveraged_VMin2.txt","OH-2-47");
+
+  // ReadSingleFile("Data/20160315-1448_Unaveraged_VMin1.txt","OH-1-3");
+  // ReadSingleFile("Data/20160315-1448_Unaveraged_VMin2.txt","OH-2-6");
 
   ReadSingleFile("Data/20160316-1220_Unaveraged_VMin1.txt","OH-1-47");
   ReadSingleFile("Data/20160316-1220_Unaveraged_VMin2.txt","OH-1-46");
 
   ReadSingleFile("Data/20160318-1911_Unaveraged_VMin1.txt","OH-1-2");
+  ReadSingleFile("Data/20160318-1911_Unaveraged_VMin2.txt","OH-1-46");
+
+  // ReadSingleFile("Data/20160323-1647_Unaveraged_VMin1.txt","OH-1-3");
+  // ReadSingleFile("Data/20160323-1647_Unaveraged_VMin2.txt","OH-2-6");
+
+  ReadSingleFile("Data/20160323-2359_Unaveraged_VMin1.txt","OH-1-3"); // combined
+  ReadSingleFile("Data/20160323-2359_Unaveraged_VMin2.txt","OH-2-6"); // combined
+
 
 
 }
@@ -36,6 +47,16 @@ void ReadSingleFile(const char* filename, const char* tileid)
   for ( auto it = signal.begin(); it != signal.end(); ++it ) histo->Fill(*it);
 
   histo->Draw();
+
+  TF1 *fgumbel = new TF1("fgumbel","([0]/sqrt(6.28))*TMath::Exp(-0.5*((x-[1])/[2] + TMath::Exp(-(x-[1])/[2])))",0,50);
+  fgumbel->SetParameter(0,100);
+  fgumbel->SetParameter(1,5.0);
+  fgumbel->SetParameter(2,5.0);
+  fgumbel->SetParLimits(0,0,1e10);
+  fgumbel->SetParLimits(1,0,1e2);
+  fgumbel->SetParLimits(2,0,1e2);
+
+  histo->Fit(fgumbel,"","",5,35);
 
   c1->Print(Form("Cosmics_%s.png",tileid));
 
