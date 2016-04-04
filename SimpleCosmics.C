4c601072f95a29bac136b1262dfd082cc41c53d4
Ron Belmont <belmonrj@gmail.com>
2016-04-04 10:55:00 -0600

    Added TLatex objects for information, and a few other minor changes

--- SimpleCosmics.C
+++ SimpleCosmics.C
@@ -8,22 +8,14 @@ void SimpleCosmics()
   ReadSingleFile("Data/20160314-1215_Unaveraged_VMin1.txt","OH-2-46");
   ReadSingleFile("Data/20160314-1215_Unaveraged_VMin2.txt","OH-2-47");
 
-  // ReadSingleFile("Data/20160315-1448_Unaveraged_VMin1.txt","OH-1-3");
-  // ReadSingleFile("Data/20160315-1448_Unaveraged_VMin2.txt","OH-2-6");
-
   ReadSingleFile("Data/20160316-1220_Unaveraged_VMin1.txt","OH-1-47");
   ReadSingleFile("Data/20160316-1220_Unaveraged_VMin2.txt","OH-1-46");
 
   ReadSingleFile("Data/20160318-1911_Unaveraged_VMin1.txt","OH-1-2");
   ReadSingleFile("Data/20160318-1911_Unaveraged_VMin2.txt","OH-1-46");
 
-  // ReadSingleFile("Data/20160323-1647_Unaveraged_VMin1.txt","OH-1-3");
-  // ReadSingleFile("Data/20160323-1647_Unaveraged_VMin2.txt","OH-2-6");
-
   ReadSingleFile("Data/20160323-2359_Unaveraged_VMin1.txt","OH-1-3"); // combined
   ReadSingleFile("Data/20160323-2359_Unaveraged_VMin2.txt","OH-2-6"); // combined
-
-
 
 }
 
@@ -46,6 +38,8 @@ void ReadSingleFile(const char* filename, const char* tileid)
 
   for ( auto it = signal.begin(); it != signal.end(); ++it ) histo->Fill(*it);
 
+  histo->GetYaxis()->SetTitle("Counts");
+  histo->GetXaxis()->SetTitle("Number of photoelectrons");
   histo->Draw();
 
   TF1 *fgumbel = new TF1("fgumbel","([0]/sqrt(6.28))*TMath::Exp(-0.5*((x-[1])/[2] + TMath::Exp(-(x-[1])/[2])))",0,50);
@@ -56,10 +50,31 @@ void ReadSingleFile(const char* filename, const char* tileid)
   fgumbel->SetParLimits(1,0,1e2);
   fgumbel->SetParLimits(2,0,1e2);
 
-  histo->Fit(fgumbel,"","",5,35);
+  histo->Fit(fgumbel,"Q","",5,35);
 
-  c1->Print(Form("Cosmics_%s.png",tileid));
+  TLatex *tex_tid = new TLatex(0.6,0.8,Form("Tile ID %s",tileid));
+  tex_tid->SetNDC();
+  tex_tid->Draw();
 
+  TLatex *tex_mpv = new TLatex(0.6,0.75,Form("MPV = %.1f #pm %.1f",fgumbel->GetParameter(1),fgumbel->GetParError(1)));
+  tex_mpv->SetNDC();
+  tex_mpv->Draw();
+
+  TLatex *tex_sig = new TLatex(0.6,0.7,Form("#sigma = %.1f #pm %.1f",fgumbel->GetParameter(2),fgumbel->GetParError(2)));
+  tex_sig->SetNDC();
+  tex_sig->Draw();
+
+  TLatex *tex_chi = new TLatex(0.6,0.65,Form("#chi^{2}/ndf = %.1f/%d",fgumbel->GetChisquare(),fgumbel->GetNDF()));
+  tex_chi->SetNDC();
+  tex_chi->Draw();
+
+  c1->Print(Form("Figs/Cosmics_%s.pdf",tileid));
+  c1->Print(Form("Figs/Cosmics_%s.png",tileid));
+
+  delete tex_tid;
+  delete tex_mpv;
+  delete tex_sig;
+  delete tex_chi;
   delete histo;
   delete c1;
 
