687541f2efcb2bf03f4352f4afe092d7671a3593
Ron Belmont <belmonrj@gmail.com>
2016-05-09 16:37:31 -0600

    Updated TileMeasurements with the rest of the data strings and added labels for plotting

--- TileMeasurements.C
+++ TileMeasurements.C
@@ -1,3 +1,7 @@
+void argument(TString, int, bool);
+
+void argument(TString, int);
+
 void argument(TString);
 
 void TileMeasurements()
@@ -11,26 +15,64 @@ void TileMeasurements()
   argument("OH-2-46");
   argument("OH-2-47");
 
+  argument("OH-1-2",1);
+  argument("OH-1-3",1);
+  // argument("OH-1-46",1,true);
+  // argument("OH-1-47",1,true);
+  argument("OH-1-46",1);
+  argument("OH-1-47",1);
+  argument("OH-2-6",1);
+  argument("OH-2-46",1);
+  argument("OH-2-47",1);
+
 }
 
 void argument(TString arg)
 {
 
+  argument(arg,0,false);
+
+}
+
+
+void argument(TString arg, int extra)
+{
+
+  argument(arg,extra,false);
+
+}
+
+
+void argument(TString arg, int extra, bool speak)
+{
+
   TCanvas *c1 = new TCanvas();
 
   TString innerfile = "Data/";
-  //innerfile += "Straight_Edge_";
-  innerfile += "Slant_Edge_"; // Sebastian labeled them backwards
-  innerfile += arg;
-  innerfile += ".txt";
-
   TString outerfile = "Data/";
-  //outerfile += "Slant_Edge_";
-  outerfile += "Straight_Edge_"; // Sebastian labeled them backwards
-  outerfile += arg;
-  outerfile += ".txt";
-
-  //cout << innerfile << " " << outerfile << endl;
+  if ( extra == 0 )
+    {
+      //outerfile += "Slant_Edge_";
+      outerfile += "Straight_Edge_"; // Sebastian labeled them backwards
+      outerfile += arg;
+      outerfile += ".txt";
+      //innerfile += "Straight_Edge_";
+      innerfile += "Slant_Edge_"; // Sebastian labeled them backwards
+      innerfile += arg;
+      innerfile += ".txt";
+    }
+  if ( extra == 1 )
+    {
+      outerfile += arg;
+      //outerfile += "_Slant";
+      outerfile += "_Straight"; // Sebastian probably labeled them backwards again
+      outerfile += ".txt";
+      innerfile += arg;
+      //innerfile += "_Straight";
+      innerfile += "_Slant"; // Sebastian probably labeled them backwards again
+      innerfile += ".txt";
+    }
+  if ( speak) cout << innerfile << " " << outerfile << endl;
 
   ifstream f_inner(innerfile);
   ifstream f_outer(outerfile);
@@ -46,13 +88,17 @@ void argument(TString arg)
       position_outer[i] = 80 - i*2;
       f_inner >> thickness_inner[i];
       f_outer >> thickness_outer[i];
+      //if ( speak ) cout << i << " " << position_outer[i] << " " << thickness_outer[i] << " " << position_inner[i] << " " << thickness_inner[i] << endl;
+      if ( speak ) cout << i << " " << position_outer[i] << " " << thickness_outer[i] << " " << thickness_inner[i] << endl;
     }
+  f_inner.close();
+  f_outer.close();
 
   TGraph *tg_inner = new TGraph(40,position_inner,thickness_inner);
   tg_inner->SetMarkerColor(kBlue);
   tg_inner->SetMarkerStyle(kFullCircle);
   tg_inner->Draw("apl");
-  tg_inner->SetMinimum(7.0);
+  tg_inner->SetMinimum(6.8);
   tg_inner->SetMaximum(9.0);
   tg_inner->GetXaxis()->SetLimits(-4,84);
   tg_inner->GetXaxis()->SetTitle("Position (cm)");
@@ -71,8 +117,10 @@ void argument(TString arg)
   tex_tid->SetNDC();
   tex_tid->Draw();
 
-  c1->Print(Form("Figs/Thickness_%s.png",(const char*)arg));
-  c1->Print(Form("Figs/Thickness_%s.pdf",(const char*)arg));
+  // c1->Print(Form("Figs/Thickness_%s.png",(const char*)arg));
+  // c1->Print(Form("Figs/Thickness_%s.pdf",(const char*)arg));
+  c1->Print(Form("Figs/Thickness_%s_%d.png",(const char*)arg,extra));
+  c1->Print(Form("Figs/Thickness_%s_%d.pdf",(const char*)arg,extra));
 
 
 
