c87e83193f2d5a4a75917ab2af4b74ac53dbb891
Ron Belmont <belmonrj@gmail.com>
2017-05-04 13:58:49 -0600

    Changed data and figures locations for SimpleCosmics

--- SimpleCosmics.C
+++ SimpleCosmics.C
@@ -5,26 +5,26 @@ void ReadSingleFile(const char*, const char*);
 void SimpleCosmics()
 {
 
-  // ReadSingleFile("Data/20160316-1220_Unaveraged_VMin1.txt","OH-1-47");
-  // ReadSingleFile("Data/20160316-1220_Unaveraged_VMin2.txt","OH-1-46");
+  // ReadSingleFile("Data/BigPanel/20160316-1220_Unaveraged_VMin1.txt","OH-1-47");
+  // ReadSingleFile("Data/BigPanel/20160316-1220_Unaveraged_VMin2.txt","OH-1-46");
 
-  // ReadSingleFile("Data/20160318-1911_Unaveraged_VMin1.txt","OH-1-2");
-  // ReadSingleFile("Data/20160318-1911_Unaveraged_VMin2.txt","OH-1-46");
+  // ReadSingleFile("Data/BigPanel/20160318-1911_Unaveraged_VMin1.txt","OH-1-2");
+  // ReadSingleFile("Data/BigPanel/20160318-1911_Unaveraged_VMin2.txt","OH-1-46");
 
-  // ReadSingleFile("Data/20160323-2359_Unaveraged_VMin1.txt","OH-1-3"); // combined
-  // ReadSingleFile("Data/20160323-2359_Unaveraged_VMin2.txt","OH-2-6"); // combined
+  // ReadSingleFile("Data/BigPanel/20160323-2359_Unaveraged_VMin1.txt","OH-1-3"); // combined
+  // ReadSingleFile("Data/BigPanel/20160323-2359_Unaveraged_VMin2.txt","OH-2-6"); // combined
 
-  // ReadSingleFile("Data/20160324-2359_Unaveraged_VMin1.txt","OH-2-46"); // combined
-  // ReadSingleFile("Data/20160324-2359_Unaveraged_VMin2.txt","OH-2-47"); // combined
+  // ReadSingleFile("Data/BigPanel/20160324-2359_Unaveraged_VMin1.txt","OH-2-46"); // combined
+  // ReadSingleFile("Data/BigPanel/20160324-2359_Unaveraged_VMin2.txt","OH-2-47"); // combined
 
-  //ReadSingleFile("Data/20151023-1307_Unaveraged_VMin1.txt","Previous");
-  ReadSingleFile("Data/20160516-1006_Unaveraged_VMin2.txt","OH-1-47");
-  ReadSingleFile("Data/20160520-1710_Unaveraged_VMin2.txt","OH-1-46");
-  ReadSingleFile("Data/20160602-1416_Unaveraged_VMin2.txt","OH-2-47");
-  ReadSingleFile("Data/20160606-1400_Unaveraged_VMin2.txt","OH-2-46");
-  ReadSingleFile("Data/20160614-1630_Unaveraged_VMin2.txt","OH-1-2");
-  ReadSingleFile("Data/20160616-1731_Unaveraged_VMin2.txt","OH-1-3");
-  ReadSingleFile("Data/20160624-1010_Unaveraged_VMin2.txt","OH-2-6");
+  //ReadSingleFile("Data/BigPanel/20151023-1307_Unaveraged_VMin1.txt","Previous");
+  ReadSingleFile("Data/BigPanel/20160516-1006_Unaveraged_VMin2.txt","OH-1-47");
+  ReadSingleFile("Data/BigPanel/20160520-1710_Unaveraged_VMin2.txt","OH-1-46");
+  ReadSingleFile("Data/BigPanel/20160602-1416_Unaveraged_VMin2.txt","OH-2-47");
+  ReadSingleFile("Data/BigPanel/20160606-1400_Unaveraged_VMin2.txt","OH-2-46");
+  ReadSingleFile("Data/BigPanel/20160614-1630_Unaveraged_VMin2.txt","OH-1-2");
+  ReadSingleFile("Data/BigPanel/20160616-1731_Unaveraged_VMin2.txt","OH-1-3");
+  ReadSingleFile("Data/BigPanel/20160624-1010_Unaveraged_VMin2.txt","OH-2-6");
 
 
 
@@ -34,6 +34,7 @@ void ReadSingleFile(const char* filename, const char* tileid)
 {
 
   TCanvas *c1 = new TCanvas();
+  gStyle->SetOptStat(0);
 
   ifstream fin(filename);
 
@@ -81,8 +82,8 @@ void ReadSingleFile(const char* filename, const char* tileid)
   tex_chi->SetNDC();
   tex_chi->Draw();
 
-  c1->Print(Form("Figs/Cosmics_%s.pdf",tileid));
-  c1->Print(Form("Figs/Cosmics_%s.png",tileid));
+  c1->Print(Form("Figures/Cosmics_%s.pdf",tileid));
+  c1->Print(Form("Figures/Cosmics_%s.png",tileid));
 
   delete tex_tid;
   delete tex_mpv;
